4c3d5d7bb8041a0ba13d00edf2cef7f9a98d456e
unknown <thakurtusharm@gmail.com>
2018-11-18 09:59:45 +0530

    first commit

---
+++ src/main/java/com/softedge/demo/model/Employee.java
@@ -0,0 +1,67 @@
+package com.softedge.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+
+	@Id
+	@Column(name="employee_id")
+	private int employeeId;
+	
+	@Column(name="employee_name")
+	private String employeeName;
+	
+	@Column(name="employee_department")
+	private String employeeDepartment;
+
+	@Column(name="employee_salary")
+	private double employeeSalary;
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(int employeeId, String employeeName, String employeeDepartment, double employeeSalary) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeDepartment = employeeDepartment;
+		this.employeeSalary = employeeSalary;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeDepartment() {
+		return employeeDepartment;
+	}
+	public void setEmployeeDepartment(String employeeDepartment) {
+		this.employeeDepartment = employeeDepartment;
+	}
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeDepartment="
+				+ employeeDepartment + ", employeeSalary=" + employeeSalary + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/softedge/demo/util/SessionFactoryProvider.java
@@ -0,0 +1,26 @@
+package com.softedge.demo.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.softedge.demo.model.Employee;
+
+public class SessionFactoryProvider {
+
+	
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	static {
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure().build();
+		sessionFactory = new MetadataSources(registry)
+				.addAnnotatedClass(Employee.class)
+				.buildMetadata().buildSessionFactory();
+	}
+}
